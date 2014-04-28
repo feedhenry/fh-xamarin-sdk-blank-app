ae89243a3d3f69156848419fd3196c7e23e34ab0
Wei Li <wei.li@feedhenry.com>
2014-04-28 15:56:59 +0100

    initial commit for xamarin templates

---
+++ FHXamarinStarterApp/App.WP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using App.WP.Resources;
+
+namespace App.WP
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
